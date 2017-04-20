06c07f4c5a3de0853a795f2086c071719035a8c6
521daichen <543577508@qq.com>
2017-04-20 16:52:08 +0800

    aa

--- customer.php
+++ customer.php
@@ -7,18 +7,21 @@
  */
 
 
-require_once 'redis.php';
+
+
+//require_once 'redis.php';
 
 require_once 'mysql.php';
 
 //模拟用户 和redis交互
 $mysql = new mysqler();
 
+//$arr = [1,2,3];
 
-$data = $database->select("platenum", [
-    "LIMIT" => [0, 1000]
-]);
+//var_dump($mysql);
 
+$data = $mysql->query("SELECT * FROM platenum limit 0,100")->fetchAll();
+//$data = $mysql->query("select * from platenum limit 0,100 ")->fetchAll();
 var_dump($data);
 
 //$datas = $mysql->select("platenum", "LIMIT" =>array(0,20));
