c5477ed8d4b3004fc273b300fa5975536f2edbed
521daichen <543577508@qq.com>
2017-04-20 17:04:01 +0800

    Merge branch 'master' of https://github.com/521daichen/redispro
    # Conflicts:
    #	customer.php

--- mysql.php
+++ mysql.php
@@ -5,11 +5,9 @@
  * Date: 2017/4/20
  * Time: 16:05
  */
-
 require_once 'model/medoo.php';
-
-class mysqler{
-    public $option = array(
+function mysql_conn(){
+    $option = array(
         'database_type' => 'mysql',
         'database_name' => 'messagequeue',
         'server' => 'localhost',
@@ -17,9 +15,24 @@ class mysqler{
         'password' => 'dc1995310',
         'charset' => 'utf8'
     );
-    public $mysql;
-    public function __construct(){
-        return new medoo($this->option);
-    }
-
+    $mysql = new medoo($this->option);
+    return $mysql;
 }
+
+
+//
+//class mysqler{
+//    public $option = array(
+//        'database_type' => 'mysql',
+//        'database_name' => 'messagequeue',
+//        'server' => 'localhost',
+//        'username' => 'root',
+//        'password' => 'dc1995310',
+//        'charset' => 'utf8'
+//    );
+//    public $mysql;
+//    public function __construct(){
+//        return new medoo($this->option);
+//    }
+//
+//}
