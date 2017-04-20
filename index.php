aabc16cc731913846b37caf9556985dc991e6eb1
521daichen <543577508@qq.com>
2017-04-20 19:14:37 +0800

    Merge branch 'master' of https://github.com/521daichen/redispro
    # Conflicts:
    #	customer.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-	$redis = new Redis();
-	$redis->connect("localhost","6379");
-	$redis->set("key","adsdaaaa");
-	$value=$redis->get("key");
-	echo $value;
-	$redis->close();
+    phpinfo();
+//	$redis = new Redis();
+//	$redis->connect("localhost","6379");
+//	$redis->set("key","adsdaaaa");
+//	$value=$redis->get("key");
+//	echo $value;
+//	$redis->close();
 
 
 
