47017c4c9e21ad19eabebc0791007be2bcedb9f8
521daichen <543577508@qq.com>
2017-04-20 19:12:36 +0800

    Merge branch 'master' of https://github.com/521daichen/redispro
    # Conflicts:
    #	customer.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-//	$redis = new Redis();
-//	$redis->connect("localhost","6379");
-//	$redis->set("key","adsdaaaa");
-//	$value=$redis->get("key");
-//	echo $value;
-//	$redis->close();
+	$redis = new Redis();
+	$redis->connect("localhost","6379");
+	$redis->set("key","adsdaaaa");
+	$value=$redis->get("key");
+	echo $value;
+	$redis->close();
 
 
 
 //模拟主程序 处理用户
-
-require_once 'redis.php';
-require_once 'mysql.php';
-header("Content-Type: text/html; charset=UTF-8");
-
-$redis = new Rediser();
+//
+//require_once 'redis.php';
+//require_once 'mysql.php';
+//header("Content-Type: text/html; charset=UTF-8");
+//
+//$redis = new Rediser();
 //$mysql=mysql_conn();
 //$listname='daichenlists';
 //
