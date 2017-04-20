eabbd28d3b6b82e509a6a4d94df7a0b4d8edd805
521daichen <543577508@qq.com>
2017-04-20 17:14:37 +0800

    Merge branch 'master' of https://github.com/521daichen/redispro
    # Conflicts:
    #	customer.php

--- customer.php
+++ customer.php
@@ -5,12 +5,14 @@
  * Date: 2017/4/20
  * Time: 17:00
  */
-
+header("Content-Type: text/html; charset=UTF-8");
 require_once 'mysql.php';
 
 $mysql =mysql_conn();
 
-$data = $mysql->select("platenum", '*'
+$data = $mysql->select("platenum", [
+        "LIMIT" => [0, 1000],
+    ]
 );
 
 var_dump($data);
